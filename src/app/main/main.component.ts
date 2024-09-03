b08e55f0399c2296c2dfc1f345bb7e8a34739f1e
Sodel <delaude.so@gmail.com>
2024-09-03 16:21:30 +0200

    corrections

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -11,24 +11,16 @@ declare var M: any;
 export class MainComponent {
 
   ngAfterViewInit() {
-    M.Collapsible.init(document.querySelectorAll('.collapsible'), {
-      accordion: false
-    });
+    this.initCollapsible();
+    this.initFormSelect();
+    this.initTimepicker();
+    this.initDatepicker();
+  }
 
-    M.FormSelect.init(document.querySelectorAll('.formSelect'));
-
-    M.Timepicker.init(document.querySelectorAll('.timepicker'), {
-      autoClose: true,
-      twelveHour: false,
-      i18n: {
-        cancel: 'Annuler',
-        done: 'Ok'
-      }
-    });
-
+  private initDatepicker() {
     M.Datepicker.init(document.querySelectorAll('.datepicker'), {
       autoClose: true,
-      format: 'dd/mm/yyyy',
+      format: 'dd/mm/yy',
       i18n: {
         cancel: 'Annuler',
         months: [
@@ -39,13 +31,32 @@ export class MainComponent {
           'Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin',
           'Juil', 'Août', 'Sep', 'Oct', 'Nov', 'Déc'
         ],
-
         weekdaysShort: [
           'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'
         ],
         weekdaysAbbrev: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
       }
+    });
+  }
 
+  private initTimepicker() {
+    M.Timepicker.init(document.querySelectorAll('.timepicker'), {
+      autoClose: true,
+      twelveHour: false,
+      i18n: {
+        cancel: 'Annuler',
+        done: 'Ok'
+      }
+    });
+  }
+
+  private initFormSelect() {
+    M.FormSelect.init(document.querySelectorAll('.formSelect'));
+  }
+
+  private initCollapsible() {
+    M.Collapsible.init(document.querySelectorAll('.collapsible'), {
+      accordion: false
     });
   }
 }
